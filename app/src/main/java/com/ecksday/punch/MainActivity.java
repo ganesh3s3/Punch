d4818bece9f512b4508229c97898e2f914f7caed
ganesh3s3 <miffychitu@gmail.com>
2016-04-06 08:15:06 +0530

    Successful implementation with 1 image viewer and setImageResource

--- app/src/main/java/com/ecksday/punch/MainActivity.java
+++ app/src/main/java/com/ecksday/punch/MainActivity.java
@@ -17,15 +17,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final ImageView thumbs = (ImageView) findViewById(R.id.imageView1);
-        final ImageView punch = (ImageView) findViewById(R.id.imageView2);
+        final ImageView Img = (ImageView) findViewById(R.id.imageView1);
         final Button ClickHere = (Button) findViewById(R.id.button);
 
         final Animation fadeOut = new AlphaAnimation(1,0);
         fadeOut.setStartOffset(1000);
         fadeOut.setDuration(1000);
-        thumbs.setAnimation(fadeOut);
-        punch.setAnimation(fadeOut);
+        Img.setAnimation(fadeOut);
 
         fadeOut.setAnimationListener(new Animation.AnimationListener() {
             @Override
@@ -35,8 +33,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                thumbs.setVisibility(View.GONE);
-                punch.setVisibility(View.GONE);
+                Img.setVisibility(View.GONE);
                 ClickHere.setVisibility(View.VISIBLE);
             }
 
@@ -53,11 +50,13 @@ public class MainActivity extends AppCompatActivity {
                                                     int i = r.nextInt(2);
                                                     ClickHere.setVisibility(View.GONE);
                                                     if(i==0) {
-                                                        thumbs.setVisibility(View.VISIBLE);
-                                                        thumbs.startAnimation(fadeOut);
+                                                        Img.setImageResource(R.drawable.thumbsup);
+                                                        Img.setVisibility(View.VISIBLE);
+                                                        Img.startAnimation(fadeOut);
                                                     } else {
-                                                        punch.setVisibility(View.VISIBLE);
-                                                        punch.startAnimation(fadeOut);
+                                                        Img.setImageResource(R.drawable.punch);
+                                                        Img.setVisibility(View.VISIBLE);
+                                                        Img.startAnimation(fadeOut);
                                                     }
                                          }
                                      }
